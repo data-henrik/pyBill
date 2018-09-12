ab548b3a862808ff71f85f9f869f9de6ca61a2a1
Henrik Loeser <hloeser@de.ibm.com>
2018-09-12 08:06:26 +0200

    added resource instance usage

--- pyBill.py
+++ pyBill.py
@@ -28,6 +28,8 @@ resourceGroupID=credentials.get('ResourceGroup').get('GUID')
 METERING_HOST="https://metering-reporting.ng.bluemix.net"
 METERING_URL="/v4/accounts/"+account_id+"/resource_groups/"+resourceGroupID+"/usage/"+str(now.year)+"-"+str(now.month)
 
+USAGE_URL="/v4/accounts/"+account_id+"/resource_instances/usage/"+str(now.year)+"-"+str(now.month)+"?_limit=100&_names=true"
+
 url=METERING_HOST+METERING_URL
 headers = {
     "Authorization": "{}".format(iam_token),
@@ -35,4 +37,17 @@ headers = {
     "Content-Type": "application/json"
 }
 response=requests.get(url, headers=headers)
+print "Usage for current resource group"
 print json.dumps(response.json())
+
+url=METERING_HOST+USAGE_URL
+headers = {
+    "Authorization": "{}".format(iam_token),
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+}
+response=requests.get(url, headers=headers)
+print "\n\nResource instance usage for first 100 items"
+print json.dumps(response.json())
+
+
